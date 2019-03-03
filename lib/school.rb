0f3ffb715325557bda1e1321cdb2c2b233b9d09a
Katie Blizzard <kblizzard11@gmail.com>
2019-03-03 03:46:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,8 @@
 class School 
 
-@roster = {}
-
 def initialize(name)
   @name = name 
+  @roster = {}
 end 
 
 def roster 
@@ -11,7 +10,7 @@ def roster
 end 
 
 def add_student(student_name, grade)
-
+  @roster[grade] << student_name
 end 
 
 
